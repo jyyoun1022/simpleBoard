ec496db895d4ac6cdbca4aa946cb42f43d8dbe6a
younjaeyeal <jyyoun1021@gmail.com>
2022-05-22 04:02:54 +0900

    upload 처

--- src/main/java/org/codej/domain/TodoDTO.java
+++ src/main/java/org/codej/domain/TodoDTO.java
@@ -2,12 +2,15 @@ package org.codej.domain;
 
 import java.sql.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
 public class TodoDTO {
 	
 	private String title;
+	@DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss")
 	private Date dueDate;
 	
 
