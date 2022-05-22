4c5021082f28ecd575aa5edf3126cffce1ba16e2
younjaeyeal <jyyoun1021@gmail.com>
2022-05-22 17:43:31 +0900

    Controller Exception 처

---
+++ src/main/java/org/codej/exception/CommonExceptrionAdvice.java
@@ -0,0 +1,24 @@
+package org.codej.exception;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import lombok.extern.log4j.Log4j;
+
+@ControllerAdvice
+@Log4j
+public class CommonExceptrionAdvice {
+	
+	@ExceptionHandler(Exception.class)
+	public String except(Exception e,Model model) {
+		log.error("Exception....."+e.getMessage());
+		model.addAttribute("exception",e);
+		log.error(model);
+		
+		return "error_page";
+	}
+	
+	
+
+}
